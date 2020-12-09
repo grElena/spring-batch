ab37ab3a5d8abe870a27ffdc35b68d12b3b1252b
Elena <gr.elena@mail.ru>
2020-12-10 01:02:31 +0500

    first commit

---
+++ src/main/java/com/example/demo/domain/Person.java
@@ -0,0 +1,14 @@
+package com.example.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Person {
+    private String lastName;
+    private String firstName;
+
+}
